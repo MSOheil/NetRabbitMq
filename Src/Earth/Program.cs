350631d106808329c159c86548fd48dffdc61644
MohamadSoheil Davoody <cap.m.davoodi@outlook.com>
2023-02-03 10:51:03 +0330

    Initial commit

---
+++ Src/Earth/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+};
+
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+channel.QueueDeclare(queue: "inbox", durable: false, exclusive: false, autoDelete: false,
+    arguments: null);
+
+var message = " ";
+while (message != String.Empty)
+{
+
+
+    Console.WriteLine("Message : ");
+
+    message = Console.ReadLine();
+
+
+
+
+    var body = Encoding.UTF8.GetBytes(message ?? "");
+    channel.BasicPublish(exchange: "",
+        routingKey: "inbox"
+        , basicProperties: null,
+        body: body);
+
+
+    Console.WriteLine("Message sent from [Earth] to [Moon] message is : " + message);
+}
---
+++ Src/Moon/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+};
+
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+channel.QueueDeclare(queue: "inbox", durable: false, exclusive: false, autoDelete: false,
+    arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += Consumer_Received;
+
+channel.BasicConsume(queue: "inbox", true, consumer: consumer);
+Console.ReadLine();
+static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Console.WriteLine(message);
+}
