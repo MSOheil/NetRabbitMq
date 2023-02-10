d5794efcc0f332af9a3746524284d659b74438a1
MohamadSoheil Davoody <cap.m.davoodi@outlook.com>
2023-02-10 10:08:14 +0330

    Added fanout exchange type config and tested it was completed

--- Src/Earth/Program.cs
+++ Src/Earth/Program.cs
@@ -1,42 +1,91 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 
-Console.WriteLine("Hello, World!");
 
 
 
-var factory = new ConnectionFactory
+
+
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
 {
-    HostName = "localhost",
-};
+    Console.WriteLine(" [*] Waiting for logs");
 
+    var consumer=new EventingBasicConsumer(channel);
 
-using var connection = factory.CreateConnection();
-using var channel = connection.CreateModel();
+    consumer.Received += (model, ea) =>
+    {
+        byte[] body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(" [x] {0}",message);
+    };
 
+    channel.BasicConsume("b", autoAck: true, consumer: consumer);
 
-channel.QueueDeclare(queue: "inbox", durable: false, exclusive: false, autoDelete: false,
-    arguments: null);
+    Console.ReadLine();
 
-var message = " ";
-while (message != String.Empty)
-{
-
-
-    Console.WriteLine("Message : ");
-
-    message = Console.ReadLine();
-
-
-
-
-    var body = Encoding.UTF8.GetBytes(message ?? "");
-    channel.BasicPublish(exchange: "",
-        routingKey: "inbox"
-        , basicProperties: null,
-        body: body);
-
-
-    Console.WriteLine("Message sent from [Earth] to [Moon] message is : " + message);
 }
+
+
+//Console.WriteLine("Hello, World!");
+
+
+
+//var factory = new ConnectionFactory
+//{
+//    HostName = "localhost",
+//};
+
+
+//using var connection = factory.CreateConnection();
+//using var channel = connection.CreateModel();
+
+//channel.ExchangeDeclare("fanout-exchange", ExchangeType.Fanout);
+//channel.QueueDeclare("fanout-queue", durable: true, exclusive: true, autoDelete: false, arguments: null);
+
+
+//channel.QueueBind("fanout-queue", "fanout-exchange", String.Empty);
+//channel.BasicQos(0, 10, false);
+
+
+//var consumer = new EventingBasicConsumer(channel);
+//consumer.Received += (sender, args) =>
+//{
+//    var body = args.Body.ToArray();
+//    var message = Encoding.UTF8.GetString(body);
+//    Console.WriteLine(message);
+//};
+
+//channel.BasicConsume("fanout-queue", true, consumer);
+//Console.WriteLine("Consumer Started");
+//Console.ReadKey();
+
+//channel.QueueDeclare(queue: "inbox", durable: false, exclusive: false, autoDelete: false,
+//    arguments: null);
+
+//var message = " ";
+//while (message != String.Empty)
+//{
+
+
+//    Console.WriteLine("Message : ");
+
+//    message = Console.ReadLine();
+
+
+
+
+//    var body = Encoding.UTF8.GetBytes(message ?? "");
+//    channel.BasicPublish(exchange: "",
+//        routingKey: "inbox"
+//        , basicProperties: null,
+//        body: body);
+
+
+//    Console.WriteLine("Message sent from [Earth] to [Moon] message is : " + message);
+//}
--- Src/Moon/Program.cs
+++ Src/Moon/Program.cs
@@ -3,30 +3,63 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 
-Console.WriteLine("Hello, World!");
+var factory=new ConnectionFactory() { HostName="localhost"};
 
-
-var factory = new ConnectionFactory
+using (var connection = factory.CreateConnection())
+using(var channel=connection.CreateModel())
 {
-    HostName = "localhost",
-};
+    channel.ExchangeDeclare("logs", ExchangeType.Fanout);
 
 
-using var connection = factory.CreateConnection();
-using var channel = connection.CreateModel();
+    channel.QueueDeclare("b", durable: false, exclusive: false, autoDelete: false, null);
 
 
-channel.QueueDeclare(queue: "inbox", durable: false, exclusive: false, autoDelete: false,
-    arguments: null);
+    channel.QueueDeclare("c", durable: false, exclusive: false, autoDelete: false, null);
 
-var consumer = new EventingBasicConsumer(channel);
-consumer.Received += Consumer_Received;
 
-channel.BasicConsume(queue: "inbox", true, consumer: consumer);
-Console.ReadLine();
-static void Consumer_Received(object sender, BasicDeliverEventArgs e)
-{
-    var message = Encoding.UTF8.GetString(e.Body.ToArray());
 
-    Console.WriteLine(message);
+    channel.QueueBind("b", "logs", routingKey: "");
+    channel.QueueBind("c", "logs", routingKey: "");
+
+
+    var message = "hello from moongs";
+    var body=Encoding.UTF8.GetBytes(message);
+
+
+    channel.BasicPublish("logs","",basicProperties:null,body);
+
+    Console.WriteLine(" [x] Send from {0}", message);
 }
+Console.WriteLine("Please enter to exit");
+Console.ReadKey();
+
+
+
+//Console.WriteLine("Hello, World!");
+
+
+//var factory = new ConnectionFactory
+//{
+//    HostName = "localhost",
+//};
+
+
+//using var connection = factory.CreateConnection();
+//using var channel = connection.CreateModel();
+
+
+
+//channel.QueueDeclare(queue: "fanout-queue", durable: false, exclusive: false, autoDelete: false,
+//    arguments: null);
+
+//var consumer = new EventingBasicConsumer(channel);
+//consumer.Received += Consumer_Received;
+
+//channel.BasicConsume(queue: "fanout-queue", true, consumer: consumer);
+//Console.ReadLine();
+//static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+//{
+//    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+//    Console.WriteLine(message);
+//}
